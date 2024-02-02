88095122e9e7069e953f31907e9db071fb4c3fb4
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-02-02 14:59:27 -0800

    x86-64 build script

--- build.sh
+++ build.sh
@@ -1,15 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-CURR_FOLDER=$(basename "$PWD")
+set -e
+rm -f -- *.o
 
-# Find all .asm files in the project folder
-for file in *.asm; do
-    # Compile each .asm file using YASM
-    yasm -g dwarf2 -f elf64 "${file}" -l "${file%.asm}.lst"
+output="$(basename "$PWD").out"
+assignments=$(find . -mindepth 1 -maxdepth 1 ! -name ".git" -type d -exec basename {} \;)
+
+echo "Choose an assignment to build:"
+select a in $assignments; do
+  # Must select the number, not the directory name, of the assignment
+  # you wish to build 
+  if [ -n "$a" ]; then
+    break
+  else
+    echo "Invalid selection. Try again."
+  fi
+done
+
+# Build the assignment
+for file in "${a}"/*.asm; do
+  yasm -g dwarf2 -f elf64 "${file}" -l "${file}.lst"
 done
 
 # Link all object files into an executable
-ld -g -o ${CURR_FOLDER} *.o
+echo "${output}"
+ld -g -o "${output}" *.o
 
 # Clean up object files
-rm *.o
+rm -f -- *.o
